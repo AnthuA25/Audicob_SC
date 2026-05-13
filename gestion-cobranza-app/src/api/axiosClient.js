13d67a3a4621ddc0aa00ce3f85bfc96655e5fafe
Aylin Santa Cruz Vargas <aylin_santacruz@hotmail.com>
2026-05-13 03:23:26 -0500

    fix: agregando variables api_url

--- gestion-cobranza-app/src/api/axiosClient.js
+++ gestion-cobranza-app/src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken, clearSession } from "../utils/storage";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5138/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   headers: {
     "Content-Type": "application/json",
   },
