44ad59bc5c8846c5a45bb8d6d98cd5d23ccbb2cc
Aylin Santa Cruz Vargas <aylin_santacruz@hotmail.com>
2026-05-14 16:58:22 -0500

    fix: cors render frontend

--- GestionCobranza-backend/Program.cs
+++ GestionCobranza-backend/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("ReactPolicy", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173", "http://localhost:3000")
+            .WithOrigins("http://localhost:5173", "http://localhost:3000","https://audicob-sc-1.onrender.com")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
