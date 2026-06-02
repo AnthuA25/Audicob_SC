61d67f1aa934d130918b15f6de74536b5920c005
cjjos <cjjosmer@gmail.com>
2026-06-02 00:22:35 -0500

    feat: implementar endpoints para HU-16 backend de alertas y notificaciones

---
+++ GestionCobranza-backend/Dtos/Alertas/ResumenAlertasDto.cs
@@ -0,0 +1,8 @@
+namespace GestionCobranza_backend.Dtos.Alertas;
+
+public class ResumenAlertasDto
+{
+    public int RiesgoMedio { get; set; }
+    public int RiesgoAlto { get; set; }
+    public int Critico { get; set; }
+}
